79edcf01ff99eddbc052cc89900643ae6b56fe8f
EC2 Default User <ec2-user@ip-172-31-66-188.ec2.internal>
2021-06-12 03:15:47 +0000

    continued4

--- bookers/app/controllers/books_controller.rb
+++ bookers/app/controllers/books_controller.rb
@@ -8,8 +8,13 @@ class BooksController < ApplicationController
  def create
   book = Book.new(book_params)
   book.save
-  redirect_to '/books'
+  redirect_to book_path(book.id)
  end
+ 
+ def show
+  @book = Book.find(params[:id])
+ end
+
   
  private
  def book_params
