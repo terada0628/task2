172896bd7fdc16804657d9a062bc4f8a658d17c4
EC2 Default User <ec2-user@ip-172-31-66-188.ec2.internal>
2021-06-13 15:31:05 +0000

    Finish1

--- bookers/app/models/book.rb
+++ bookers/app/models/book.rb
@@ -1,4 +1,4 @@
 class Book < ApplicationRecord
-    validates :title, presence: { message: "no text error" }
-    validates :body, presence: { message: "no text error" }
+    validates :title, presence: true
+    validates :body, presence: true
 end
